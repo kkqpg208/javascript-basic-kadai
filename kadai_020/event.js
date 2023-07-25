adb6600cd43cf708e5ae941165c87762bd3d0391
kkqpg208 <138633424+kkqpg208@users.noreply.github.com>
2023-07-25 12:54:31 +0900

    kadai_020を提出

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+//btn, textのHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', ()=> {
+  setTimeout(() => {
+    //テキストを書き換える
+    text.textContent = 'ボタンをクリックしました';    
+  }, 2000);  
+})
