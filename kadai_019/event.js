b4f98222d82c502b4e93c5442fa6313d57c1640a
kkqpg208 <138633424+kkqpg208@users.noreply.github.com>
2023-07-25 12:50:13 +0900

    kadai_019を再提出

--- kadai_019/event.js
+++ kadai_019/event.js
@@ -1,17 +1,9 @@
-//btn, text, bodyのHTML要素を取得し、定数に代入する
+//btn, textのHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
-const body = document.getElementById('body');
 
-// HTML要素がクリックされたときにイベント処理を実行する
+// btnのHTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', ()=> {
-  //新しくh2要素を作成し、定数に代入する
-  const text2 = document.createElement('h2');
-  //作成したh2要素にテキストを追加する
-  text2.textContent = 'ボタンをクリックしました';
-  //body要素の末尾にh2要素を追加する
-  document.querySelector('body').appendChild(text2);
-  //「ボタンをクリックしてください」を消す
-  text.style.display = 'none';
-  
+  //文章を書き換える
+  text.textContent = 'ボタンをクリックしました';
 })
